24bfb7031d7cc38bddeedd6da59e9ecbc7753a03
petekostrytsya <petekostrytsya@gmail.com>
2023-04-14 22:59:18 +0300

    addstart

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputCountry = document.querySelector('#search-box');
+const infoCountry = document.querySelector('.country-info');
+const listCountry = document.querySelector('.country-list');
+
+infoCountry.addEventListener('input', debounce(inputSearch, DEBOUNCE_DELAY));
+function inputSearch(evt) {
+    evt.prevendDefault();
+    const searchCountries = evt.target.value.trim();
+    if (!searchCountries) {
+        listCountry.innerHTML = '';
+        infoCountry.innerHTML = '';
+        return;
+    }
+}
